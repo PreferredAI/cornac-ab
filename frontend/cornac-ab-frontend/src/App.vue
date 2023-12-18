c277c3e49203db3eddae3be797b708e99b77a82b
darrylong <darrylong@users.noreply.github.com>
2023-12-18 17:17:22 +0800

    design changes

--- frontend/cornac-ab-frontend/src/App.vue
+++ frontend/cornac-ab-frontend/src/App.vue
@@ -1,26 +1,67 @@
-<script setup>
+<!-- <script setup>
 // import HelloWorld from './components/HelloWorld.vue'
 import Welcome from './views/Welcome.vue'
-</script>
+</script> -->
 
 <template>
-  <!-- <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div> -->
-  <!-- <HelloWorld msg="Vite + Vue" /> -->
-  <div>
-    <router-link to="/">Welcome</router-link> |
-    <router-link to="/setup">Setup</router-link> |
-    <router-link to="/hello">Hello</router-link> |
-    <router-link to="/dashboard">Dashboard</router-link>
+  <!--
+    This example requires updating your template:
+
+    ```
+    <html class="h-full bg-gray-100">
+    <body class="h-full">
+    ```
+  -->
+  <div class="min-h-full">
+  <Disclosure as="nav" class="bg-gray-800" v-slot="{ open }">
+      <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+      <div class="flex h-16 items-center justify-between">
+          <div class="flex items-center">
+          <div class="hidden md:block">
+              <div class="ml-10 flex items-baseline space-x-4">
+              <a v-for="item in navigation" :key="item.name" :href="item.href" :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'rounded-md px-3 py-2 text-sm font-medium']" :aria-current="item.current ? 'page' : undefined">{{ item.name }}</a>
+              </div>
+          </div>
+          </div>
+          <div class="-mr-2 flex md:hidden">
+          <!-- Mobile menu button -->
+          <DisclosureButton class="relative inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+              <span class="absolute -inset-0.5" />
+              <span class="sr-only">Open main menu</span>
+              <Bars3Icon v-if="!open" class="block h-6 w-6" aria-hidden="true" />
+              <XMarkIcon v-else class="block h-6 w-6" aria-hidden="true" />
+          </DisclosureButton>
+          </div>
+      </div>
+      </div>
+
+      <DisclosurePanel class="md:hidden">
+      <div class="space-y-1 px-2 pb-3 pt-2 sm:px-3">
+          <DisclosureButton v-for="item in navigation" :key="item.name" as="a" :href="item.href" :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'block rounded-md px-3 py-2 text-base font-medium']" :aria-current="item.current ? 'page' : undefined">{{ item.name }}</DisclosureButton>
+      </div>
+      </DisclosurePanel>
+  </Disclosure>
+
+  <main>
+      <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+        <router-view />
+      </div>
+  </main>
   </div>
-  <router-view />
 </template>
+
+<script setup>
+import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
+// import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/vue/24/outline'
+
+const navigation = [
+  { name: 'Welcome', href: '/', current: false },
+  { name: 'Setup', href: '/setup', current: false },
+  { name: 'Dashboard', href: '/dashboard', current: false },
+  { name: 'Evaluation', href: '/evaluation', current: false },
+]
+</script>
+
 
 <style scoped>
 .logo {
--- frontend/cornac-ab-frontend/src/views/Dashboard.vue
+++ frontend/cornac-ab-frontend/src/views/Dashboard.vue
@@ -1,8 +1,18 @@
 <template>
+    <header class="bg-white shadow">
+        <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+        <h1 class="text-3xl font-bold tracking-tight text-gray-900">Dashboard</h1>
+        </div>
+    </header>
     <div>
-        <h1 class="text-2xl font-bold mb-4">Dashboard</h1>
         <div class="mb-4">
             <button class="mt-2 bg-blue-500 text-white px-4 py-2 rounded-md">Run Evaluation</button>
+        </div>
+        <div>
+            comments: possible sections in this dashboard:
+            - ability to control time frame of recommendations and feedback given by users
+            - ability to show popular items recommended to users
+            - items with positive feedback given by users
         </div>
         <iframe src="http://0.0.0.0:5601/app/dashboards#/view/722b74f0-b882-11e8-a6d9-e546fe2bba5f?embed=true&_a=(description:'Analyze%20mock%20eCommerce%20orders%20and%20revenue',filters:!(),fullScreenMode:!f,options:(hidePanelTitles:!f,useMargins:!t),panels:!((embeddableConfig:(),gridData:(h:10,i:'1',w:36,x:12,y:18),id:'37cc8650-b882-11e8-a6d9-e546fe2bba5f',panelIndex:'1',type:visualization,version:'2.11.1'),(embeddableConfig:(vis:(legendOpen:!f)),gridData:(h:11,i:'2',w:12,x:12,y:7),id:ed8436b0-b88b-11e8-a6d9-e546fe2bba5f,panelIndex:'2',type:visualization,version:'2.11.1'),(embeddableConfig:(),gridData:(h:7,i:'3',w:18,x:0,y:0),id:'09ffee60-b88c-11e8-a6d9-e546fe2bba5f',panelIndex:'3',type:visualization,version:'2.11.1'),(embeddableConfig:(),gridData:(h:7,i:'4',w:30,x:18,y:0),id:'1c389590-b88d-11e8-a6d9-e546fe2bba5f',panelIndex:'4',type:visualization,version:'2.11.1'),(embeddableConfig:(),gridData:(h:11,i:'5',w:48,x:0,y:28),id:'45e07720-b890-11e8-a6d9-e546fe2bba5f',panelIndex:'5',type:visualization,version:'2.11.1'),(embeddableConfig:(),gridData:(h:10,i:'6',w:12,x:0,y:18),id:'10f1a240-b891-11e8-a6d9-e546fe2bba5f',panelIndex:'6',type:visualization,version:'2.11.1'),(embeddableConfig:(),gridData:(h:11,i:'7',w:12,x:0,y:7),id:b80e6540-b891-11e8-a6d9-e546fe2bba5f,panelIndex:'7',type:visualization,version:'2.11.1'),(embeddableConfig:(vis:(legendOpen:!f)),gridData:(h:11,i:'8',w:12,x:24,y:7),id:'4b3ec120-b892-11e8-a6d9-e546fe2bba5f',panelIndex:'8',type:visualization,version:'2.11.1'),(embeddableConfig:(vis:(legendOpen:!f)),gridData:(h:11,i:'9',w:12,x:36,y:7),id:'9ca7aa90-b892-11e8-a6d9-e546fe2bba5f',panelIndex:'9',type:visualization,version:'2.11.1'),(embeddableConfig:(),gridData:(h:18,i:'10',w:48,x:0,y:54),id:'3ba638e0-b894-11e8-a6d9-e546fe2bba5f',panelIndex:'10',type:search,version:'2.11.1'),(embeddableConfig:(mapCenter:!(28.304380682962783,-22.148437500000004),mapZoom:2),gridData:(h:15,i:'11',w:24,x:0,y:39),id:'9c6f83f0-bb4d-11e8-9c84-77068524bcab',panelIndex:'11',type:visualization,version:'2.11.1'),(embeddableConfig:(),gridData:(h:15,i:'12',w:24,x:24,y:39),id:b72dd430-bb4d-11e8-9c84-77068524bcab,panelIndex:'12',type:visualization,version:'2.11.1')),query:(language:kuery,query:''),timeRestore:!t,title:'%5BeCommerce%5D%20Revenue%20Dashboard',viewMode:edit)&_g=(filters:!(),refreshInterval:(pause:!f,value:900000),time:(from:now-7d,to:now))" height="1024" width="100%"></iframe>
     </div>
