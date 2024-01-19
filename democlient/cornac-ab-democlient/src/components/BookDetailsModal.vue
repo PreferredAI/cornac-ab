54c6b243cbe0d05da686ea88a3020a00fa0f4a55
darrylong <darrylong@users.noreply.github.com>
2024-01-19 16:47:22 +0800

    Added dynamic book details for pop up

--- democlient/cornac-ab-democlient/src/components/BookDetailsModal.vue
+++ democlient/cornac-ab-democlient/src/components/BookDetailsModal.vue
@@ -2,32 +2,8 @@
 import { ref } from 'vue';
 import { XMarkIcon } from '@heroicons/vue/24/outline'
 const isContentShown = ref(false);
-const open = () => (isContentShown.value = true);
-
-const book = {
-  "book_id" : "1",
-  "goodreads_book_id" : "2767052",
-  "best_book_id":"2767052",
-  "work_id":"2792775",
-  "books_count":"272",
-  "isbn":"439023483",
-  "isbn13":"9.78043902348e+12",
-  "authors":"Suzanne Collins",
-  "original_publication_year":"2008.0",
-  "original_title":"The Hunger Games",
-  "title":"The Hunger Games (The Hunger Games, #1)",
-  "language_code":"eng",
-  "average_rating":"4.34",
-  "ratings_count":"4780653",
-  "work_ratings_count":"4942365",
-  "work_text_reviews_count":"155254",
-  "ratings_1":"66715",
-  "ratings_2":"127936",
-  "ratings_3":"560092",
-  "ratings_4":"1481305",
-  "ratings_5":"2706317",
-  "image_url":"https://images.gr-assets.com/books/1447303603l/2767052.jpg",
-  "small_image_url":"https://images.gr-assets.com/books/1447303603s/2767052.jpg"
+const open = () => {
+  isContentShown.value = true;
 };
 
 defineExpose({ open });
@@ -51,8 +27,8 @@ defineExpose({ open });
                 </button>
 
                 <div class="grid w-full grid-cols-1 items-start gap-x-6 gap-y-8 sm:grid-cols-12 lg:gap-x-8">
-                  <div class="aspect-h-3 aspect-w-2 overflow-hidden rounded-lg bg-gray-100 sm:col-span-4 lg:col-span-5">
-                    <img :src="book.image_url" :alt="Test" class="object-cover object-center" />
+                  <div class="h-full overflow-hidden rounded-lg sm:col-span-4 lg:col-span-5">
+                    <img :src="book.image_url" class="h-full w-full" />
                   </div>
                   <div class="sm:col-span-8 lg:col-span-7">
                     <h2 class="text-2xl font-bold text-gray-900 sm:pr-12">{{ book.original_title }}</h2>
@@ -61,18 +37,6 @@ defineExpose({ open });
                       <h3 id="information-heading" class="sr-only">Product information</h3>
 
                       <p class="text-2xl text-gray-900">{{ book.title }}</p>
-
-                      <!-- Reviews -->
-                      <div class="mt-6">
-                        <h4 class="sr-only">Reviews</h4>
-                        <div class="flex items-center">
-                          <div class="flex items-center">
-                            <!-- <StarIcon v-for="rating in [0, 1, 2, 3, 4]" :key="rating" :class="[product.rating > rating ? 'text-gray-900' : 'text-gray-200', 'h-5 w-5 flex-shrink-0']" aria-hidden="true" /> -->
-                          </div>
-                          <!-- <p class="sr-only">{{ product.rating }} out of 5 stars</p> -->
-                          <!-- <a href="#" class="ml-3 text-sm font-medium text-indigo-600 hover:text-indigo-500">{{ product.reviewCount }} reviews</a> -->
-                        </div>
-                      </div>
                     </section>
 
                     <section aria-labelledby="options-heading">
@@ -82,7 +46,7 @@ defineExpose({ open });
                         <div class="mt-2">
                           <h2 class="text-lg font-medium text-gray-900">Details</h2>
 
-                          <div class="mt-2 space-y-6">
+                          <div class="mt-2 space-y-2">
                             <p class="text-md text-gray-600">Authors</p>{{ book.authors }}
                             <p class="text-md text-gray-600">Year Released</p>{{ book.original_publication_year }}
                             <p class="text-md text-gray-600">Average Rating</p>{{ book.average_rating }} stars
@@ -118,5 +82,6 @@ import {
 
 export default {
     name: 'BookDetailsModal',
+    props: ['book'],
 };
 </script>
