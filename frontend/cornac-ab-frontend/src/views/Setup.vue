eb3e0b0e8c247d849a18762f675bf97ee0f8de44
darrylong <darrylong@users.noreply.github.com>
2024-01-26 14:33:45 +0800

    Update model setup height to be more compact

--- frontend/cornac-ab-frontend/src/views/Setup.vue
+++ frontend/cornac-ab-frontend/src/views/Setup.vue
@@ -40,7 +40,7 @@
 
                     <div class="lg:grid lg:grid-cols-2 lg:gap-x-6">
                         <div v-for="(model, index) in models" :key="index" class="mt-10 grid grid-cols-1">
-                            <div class="transition mt-2 justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
+                            <div class="transition mt-2 justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-5">
                                 <div class="col-span-4 mt-4">
                                     <label v-bind:for="'model-name-' + index" class="block text-sm font-medium leading-6 text-gray-900">Model name</label>
                                     <div class="mt-2">
@@ -58,7 +58,7 @@
                                 </div> -->
                                 <div class="col-span-4 mt-4">
                                     <label for="model-file" class="block text-sm font-medium leading-6 text-gray-900">Model file</label>
-                                    <label v-bind:for="'dropzone-file-' + index" class="flex mt-2 flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+                                    <label v-bind:for="'dropzone-file-' + index" class="flex mt-2 flex-col items-center justify-center w-full h-36 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
                                         <div v-if="!model.file" class="flex flex-col items-center justify-center pt-5 pb-6">
                                             <svg class="w-8 h-8 mb-2 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
                                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
