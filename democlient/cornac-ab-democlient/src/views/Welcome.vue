7147b187741cb9686faf50d72bdd92bdd5f79a05
darrylong <darrylong@users.noreply.github.com>
2024-04-17 16:38:11 +0800

    Add lazy loading, dark mode to demo client

--- democlient/cornac-ab-democlient/src/views/Welcome.vue
+++ democlient/cornac-ab-democlient/src/views/Welcome.vue
@@ -2,20 +2,20 @@
     <div class="relative isolate px-6 pt-14 lg:px-8">
       <div class="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
         <div class="hidden sm:mb-8 sm:flex sm:justify-center">
-          <div class="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-            The best books, only found here! <a href="https://cornac.preferred.ai/" class="font-semibold text-indigo-600"><span class="absolute inset-0" aria-hidden="true" />View Here <span aria-hidden="true">&rarr;</span></a>
+          <div class="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20 dark:text-gray-300 dark:ring-gray-400 dark:hover:ring-gray-100">
+            The best books, only found here! <a href="https://cornac.preferred.ai/" class="font-semibold text-indigo-600 dark:text-indigo-400"><span class="absolute inset-0" aria-hidden="true" />View Here <span aria-hidden="true">&rarr;</span></a>
           </div>
         </div>
         <div class="text-center">
-          <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Welcome to Books A-B</h1>
-          <p class="mt-6 text-lg leading-8 text-gray-600">Find the best books, rate them and get going.</p>
+          <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl dark:text-white">Welcome to Books A-B</h1>
+          <p class="mt-6 text-lg leading-8 text-gray-600 dark:text-gray-200">Find the best books, rate them and get going.</p>
           
           <div v-if="!loggedInUserid" class="mt-10 flex items-center justify-center gap-x-6">
             <form class="space" action="#" method="POST">
               <div>
-                <label for="userid" class="block text-sm font-medium leading-6 text-gray-900">Enter Username</label>
+                <label for="userid" class="block text-sm font-medium leading-6 text-gray-900 dark:text-gray-100">Enter Username</label>
                 <div class="mt-2">
-                  <input id="userid" name="userid" v-model="userid" required class="block w-full rounded-md border-0 py-1.5 px-4 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                  <input id="userid" name="userid" v-model="userid" required class="block w-full rounded-md border-0 py-1.5 px-4 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 dark:bg-gray-300">
                 </div>
               </div>
               <div class="mt-4">
@@ -24,9 +24,9 @@
             </form>
           </div>
           <div v-else class="mt-4">
-            <h3 class="font-semibold">Currently logged in as: {{ loggedInUserid }}</h3>
+            <h3 class="font-semibold dark:text-white">Currently logged in as: {{ loggedInUserid }}</h3>
             <div class="mt-4">
-              <a href="/"  type="submit" class="mx-2 rounded-md bg-indigo-600 px-8 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">View Books</a>
+              <a href="/"  type="submit" class="mx-2 rounded-md bg-indigo-600 px-8 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Explore Books</a>
               <button type="submit" @click.stop.prevent="logout()" class="mx-2 rounded-md bg-indigo-600 px-8 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Log Out</button>
             </div>
           </div>
