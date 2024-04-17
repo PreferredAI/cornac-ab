becb66d046f46aae136308844342b32b923439e4
darrylong <darrylong@users.noreply.github.com>
2024-04-17 15:29:47 +0800

    Demo client error handling

--- democlient/cornac-ab-democlient/src/components/BookDetailsModal.vue
+++ democlient/cornac-ab-democlient/src/components/BookDetailsModal.vue
@@ -55,13 +55,13 @@ defineExpose({ open });
                             <p class="text-md text-gray-600">Average Rating</p>{{ book.attributes.rating }} stars
                             <p class="text-md text-gray-600">ISBN</p>{{ book.attributes.isbn }}
                             
-                            <p v-if="this.book.selectedRating" class="text-md text-gray-600"> Rated </p>
+                            <p v-if="book.selectedRating" class="text-md text-gray-600"> Rated </p>
                             <p v-else class="text-md text-gray-600"> Rate this book </p>
-                            <div v-if="this.book.selectedRating" class="flex">
-                              <svg v-for="n in this.book.selectedRating" class="text-yellow-400" width="23" height="23" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" stroke="currentColor">
+                            <div v-if="book.selectedRating" class="flex">
+                              <svg v-for="n in book.selectedRating" class="text-yellow-400" width="23" height="23" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"></path>
                               </svg>
-                              <svg v-for="n in (5 - this.book.selectedRating)" class="text-gray-600" width="23" height="23" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" stroke="currentColor">
+                              <svg v-for="n in (5 - book.selectedRating)" class="text-gray-600" width="23" height="23" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"></path>
                               </svg>
                             </div>
@@ -100,7 +100,6 @@ export default {
     data() {
         return {
           loading: false,
-          // selectedRating: null,
         };
     },
     methods: {
